e56ddc9e28d71ad9bf975fb18eeeade34f4c804b
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2024-02-11 15:43:42 -0700

    resolving navigation script type errors, exporting dynamic properties

---
+++ src/components/AddItemCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="container">
+  <ion-card>
+        <ion-card-header>
+          <ion-card-title><strong>{{ name }}</strong></ion-card-title>
+          <ion-card-subtitle>Add a new {{ addText }} to your closet library.</ion-card-subtitle>
+          </ion-card-header>
+        <ion-button id="open-action-sheet-{{ idAppend }}">Upload</ion-button>
+        <ion-action-sheet trigger="open-action-sheet-{{ idAppend }}" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
+      </ion-card>
+      </div>
+</template>
+
+<script lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle },
+    setup() {
+      const actionSheetButtons = [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ];
+
+      return { actionSheetButtons };
+    },
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      addText: {
+        type: String,
+        required: true,
+      }
+  },
+  });
+</script>
+
+<style scoped>
+/* #container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+} */
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Add New Items to Closet</ion-title>
+        <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -11,82 +11,14 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Shirt</ion-card-title>
-          <ion-card-subtitle>Add a new shirt to your closet library.</ion-card-subtitle>
-          </ion-card-header>
-        <ion-button id="open-action-sheet-shirt">Upload</ion-button>
-        <ion-action-sheet trigger="open-action-sheet-shirt" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
-      </ion-card>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Pants</ion-card-title>
-          <ion-card-subtitle>Add a new pair of pants to your closet library.</ion-card-subtitle>
-          </ion-card-header>
-        <ion-button id="open-action-sheet-pants">Upload</ion-button>
-        <ion-action-sheet trigger="open-action-sheet-pants" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
-      </ion-card>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Skirt</ion-card-title>
-          <ion-card-subtitle>Add a new skirt to your closet library.</ion-card-subtitle>
-          </ion-card-header>
-        <ion-button id="open-action-sheet-skirt">Upload</ion-button>
-        <ion-action-sheet trigger="open-action-sheet-skirt" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
-      </ion-card>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Dress</ion-card-title>
-          <ion-card-subtitle>Add a new dress to your closet library.</ion-card-subtitle>
-          </ion-card-header>
-        <ion-button id="open-action-sheet-dress">Upload</ion-button>
-        <ion-action-sheet trigger="open-action-sheet-dress" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
-      </ion-card>
-      <ion-card>
-        <ion-card-header>
-          <ion-card-title>Shoes</ion-card-title>
-          <ion-card-subtitle>Add a new pair of shoes to your closet library.</ion-card-subtitle>
-          </ion-card-header>
-        <ion-button id="open-action-sheet-shoes">Upload</ion-button>
-        <ion-action-sheet trigger="open-action-sheet-shoes" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
-      </ion-card>
+
+      <AddItemCard name="Pants" idAppend="pants" addText="pair of pants"/>
+      <AddItemCard name="Dress" idAppend="dress" addText="dress"/>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-
-  export default defineComponent({
-    components: { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle },
-
-    setup() {
-      const actionSheetButtons = [
-        {
-          text: 'Delete',
-          role: 'destructive',
-          data: {
-            action: 'delete',
-          },
-        },
-        {
-          text: 'Share',
-          data: {
-            action: 'share',
-          },
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          data: {
-            action: 'cancel',
-          },
-        },
-      ];
-
-      return { actionSheetButtons };
-    },
-  });
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import AddItemCard from '@/components/AddItemCard.vue';
 </script>
