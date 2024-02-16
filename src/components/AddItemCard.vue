80847cd89366b94e71a17a63fa3995ed1292901e
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2024-02-16 15:17:04 -0700

    template for uploading images and handling upload image logic

--- src/components/AddItemCard.vue
+++ src/components/AddItemCard.vue
@@ -8,8 +8,9 @@
           <ion-card-subtitle>Add a new {{ addText }} to your closet library.</ion-card-subtitle>
           <TileViewPreview :tile1="tile1" :tile2="tile2" :tile3="tile3" />  
         </IonCardContent>
+          <input type="file" accept="image/*,.heic" />
           <ion-button :id="'open-action-sheet-' + tag" :disabled="!enabled">Upload</ion-button>
-        <ion-action-sheet :trigger="'open-action-sheet-' + tag" header="Actions" :buttons="actionSheetButtons"></ion-action-sheet>
+        <ion-action-sheet :trigger="'open-action-sheet-' + tag" :header="'Upload ' + tag + ' to your closet library'" :buttons="actionSheetButtons"></ion-action-sheet>
       </ion-card>
       </div>
 </template>
@@ -27,9 +28,13 @@ import { IKImage, IKContext, IKVideo, IKUpload } from "imagekitio-vue";
       const actionSheetButtons = [
         {
           text: 'Upload',
+          role: 'upload',
           data: {
             action: 'upload',
           },
+          handler: () => {
+          console.log('Upload action triggered');
+        },
         },
         {
           text: 'Cancel',
@@ -39,8 +44,15 @@ import { IKImage, IKContext, IKVideo, IKUpload } from "imagekitio-vue";
           },
         },
       ];
+      const handleAction = (action: string) => {
+      if (action === 'upload') {
+        console.log('Upload action triggered');
+      } else {
+        return;
+      }
+    };
 
-      return { actionSheetButtons };
+      return { actionSheetButtons, handleAction};
     },
     props: {
       name: {
@@ -73,31 +85,8 @@ import { IKImage, IKContext, IKVideo, IKUpload } from "imagekitio-vue";
     }
   },
   });
+  
 </script>
 
 <style scoped>
-/* #container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-} */
 </style>
