cf4d13ebfa96962dd8a44495113acc15c939534b
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2024-02-19 18:54:55 -0700

    updating to use correct variable

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,10 +1,11 @@
 // Function to retrieve JWT from storage
 export function getHeaders() {
-  const JWT = localStorage.getItem('jwt');
+  const JWT = localStorage.getItem('token');
   const config = {
     headers: {
       'Authorization': `Bearer ${JWT}` 
     }
   }
+  console.log(config)
     return config;
   }
